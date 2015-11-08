a73c4816c38a1878a81aad0e8f7cef911b8437b5
Vladimir Ivanov <icu0755@gmail.com>
2015-11-08 11:15:02 +0300

    fix start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 NAME="cleverup-crm"                                  # Name of the application
-DJANGODIR=/opt/cleverup/cleverup             # Django project directory
+DJANGODIR=/opt/cleverup/crm             # Django project directory
 SOCKFILE=/opt/run/cleverup/socket  # we will communicate using this unix socket
 USER=cleverup                                     # the user to run as
 GROUP=cleverup                                     # the group to run as
 NUM_WORKERS=2                                     # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=frontend.settings.prod
-DJANGO_WSGI_MODULE=cleverup                     # WSGI module name
+DJANGO_WSGI_MODULE=crm                     # WSGI module name
 
 echo "Starting $NAME as `whoami`"
 
@@ -23,5 +23,5 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ../env/bin/waitress-serve ${DJANGO_WSGI_MODULE}.wsgi:application \
-  --unix_socket $SOCKFILE
+exec ../env/bin/waitress-serve --unix-socket=$SOCKFILE ${DJANGO_WSGI_MODULE}.wsgi:application
+
