d81bcd6b76a4d5faf9177ee39868e97644e70988
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2015-11-08 21:33:02 +0100

    add autoclose

--- crm/frontend/static/js/script.js
+++ crm/frontend/static/js/script.js
@@ -1,5 +1,6 @@
 jQuery(function ($) {
     "use strict";
     $.fn.datepicker.defaults.format = "yyyy-mm-dd";
+    $.fn.datepicker.defaults.autoclose = true;
     $('.cu-datepicker').datepicker();
 });
